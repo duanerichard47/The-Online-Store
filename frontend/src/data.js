ec069dfc8bc2265adfef40f7043eac55dc8025ba
Duane Anderson <duanerichard47@hotmail.com>
2023-04-03 07:58:14 -0400

    lesson 13

---
+++ frontend/src/data.js
@@ -0,0 +1,53 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category:'Shirts',
+            image: '/images/p1.jpg', //679px x 829px
+            price: 120,
+            countInstock: 10,
+            brankd: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt',
+        },
+        {
+            name: 'Adidas Fit Shirt',
+            slug: 'adidas-fit-shirt',
+            category:'Shirts',
+            image: '/images/p2.jpg',
+            price: 250,
+            countInstock: 20,
+            brankd: 'Adidas',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            name: 'Nike Slim Pant',
+            slug: 'nike-slim-pant',
+            category:'Pants',
+            image: '/images/p3.jpg',
+            price: 25,
+            countInstock: 15,
+            brankd: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'high quality product',
+        },
+        {
+            name: 'Adidas Fit Pant',
+            slug: 'adidas-fit-pantt',
+            category:'Pants',
+            image: '/images/p4.jpg',
+            price: 65,
+            countInstock: 10,
+            brankd: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+    ],
+};
+export default data;
